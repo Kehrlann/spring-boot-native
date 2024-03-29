38fbbb193df98070a3c4d230e4e84f5f15175092
Daniel Garnier-Moiroux <git@garnier.wf>
2024-03-29 16:35:50 +0100

    feat: add welcome message on webserver startup, showcase serialization

--- simple/src/test/java/wf/garnier/nativedemo/starwars/StarWarsNativeTests.java
+++ simple/src/test/java/wf/garnier/nativedemo/starwars/StarWarsNativeTests.java
@@ -2,18 +2,23 @@ package wf.garnier.nativedemo.starwars;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledInNativeImage;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class StarWarsControllerNativeTests {
+@ExtendWith(OutputCaptureExtension.class)
+class StarWarsNativeTests {
 
 	@Autowired
 	MockMvc mockMvc;
@@ -25,6 +30,12 @@ class StarWarsControllerNativeTests {
 			.andExpect(status().is2xxSuccessful())
 			.andExpect(jsonPath("$.characters[0].name").value("C-3PO"))
 			.andExpect(jsonPath("$.characters[0].movieCount").value(6));
+	}
+
+	@Test
+	@EnabledInNativeImage
+	void welcomeMessage(CapturedOutput output) {
+		assertThat(output.getOut()).contains("🏃");
 	}
 
 }
