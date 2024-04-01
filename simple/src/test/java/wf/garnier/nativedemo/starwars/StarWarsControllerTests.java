a80e6712e3346e6edfd2f90450857f1d17f8c38b
Daniel Garnier-Moiroux <git@garnier.wf>
2024-04-01 22:26:54 +0200

    tests: disable starwars tests in AOT mode

--- simple/src/test/java/wf/garnier/nativedemo/starwars/StarWarsControllerTests.java
+++ simple/src/test/java/wf/garnier/nativedemo/starwars/StarWarsControllerTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.GenericContainer;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Import(StarWarsControllerTests.SwapiContainerConfiguration.class)
 @DisabledInNativeImage
+@DisabledInAotMode
 class StarWarsControllerTests {
 
 	@Autowired
