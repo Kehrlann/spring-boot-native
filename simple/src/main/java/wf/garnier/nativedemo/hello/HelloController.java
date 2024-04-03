2bf18b506a187ed577a88611f67c2e54617a3c1a
Daniel Garnier-Moiroux <git@garnier.wf>
2024-04-03 14:19:08 +0200

    feat: add /bonjour endpoint

--- simple/src/main/java/wf/garnier/nativedemo/hello/HelloController.java
+++ simple/src/main/java/wf/garnier/nativedemo/hello/HelloController.java
@@ -1,22 +1,29 @@
 package wf.garnier.nativedemo.hello;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
 	private final GreetingService greetingService;
 
-	public HelloController(GreetingService greetingService) {
+	private final String bonjour;
+
+	public HelloController(GreetingService greetingService, @Value("${greeting_bonjour:Bonjour!}") String bonjour) {
 		this.greetingService = greetingService;
+		this.bonjour = bonjour;
 	}
 
 	@GetMapping("/hello")
-	@ResponseBody
 	public String hello(String name) {
 		return greetingService.greet(name);
+	}
+
+	@GetMapping("/bonjour")
+	public String bonjour() {
+		return bonjour;
 	}
 
 }
