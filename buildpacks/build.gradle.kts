461fd094c11e1754d24b957b02bd2c45e11af625
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-06 10:02:30 +0200

    build: remove tasks from the buildpacks project

--- buildpacks/build.gradle.kts
+++ buildpacks/build.gradle.kts
@@ -1,31 +1,52 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.graalvm.buildtools.native") version "0.9.28"
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("io.spring.javaformat") version "0.0.41"
+    id("org.graalvm.buildtools.native") version "0.10.1"
+    // Note: this must match the "book" project's build script.
+    // See https://github.com/gradle/gradle/issues/17559
+    // We could put it all under the root settings, but I want the "books"
+    // project to be somewhat self-contained.
 }
 
 group = "wf.garnier"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.bootBuildImage {
-	imageName = "demo:native"
-	publish = false
+    imageName = "demo:native"
+    publish = false
+
+    // For the demo: make sure we're not building this image from the root project
+    enabled = false
+}
+
+// Disabled some tasks to avoid running them from the root project
+tasks.nativeCompile {
+    enabled = false
+}
+
+tasks.nativeTest {
+    enabled = false
+}
+
+tasks.processAot {
+    enabled = false
 }
