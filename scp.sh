64b381b738842e931a05a632abdb3c8cc9b87279
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-06 14:51:07 +0200

    build: fix scp script

--- scp.sh
+++ scp.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-gcloud compute scp --zone "europe-west9-a" --project "cf-identity-service-oak" simple/build/native/nativeCompile/simple daniel-native-vm:~/native-app-mac
+gcloud compute scp \
+  --zone "europe-west9-a" \
+  --project "cf-identity-service-oak" \
+  books/build/native/nativeCompile/books \
+  daniel-native-vm:~/native-app-mac
