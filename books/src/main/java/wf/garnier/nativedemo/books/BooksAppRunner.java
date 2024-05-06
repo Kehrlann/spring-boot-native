c54628d65aa8511274ba4f1298ca9cdc7d973c10
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-06 23:14:23 +0200

    feat: add an app runner that shows the config

---
+++ books/src/main/java/wf/garnier/nativedemo/books/BooksAppRunner.java
@@ -0,0 +1,28 @@
+package wf.garnier.nativedemo.books;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+class BooksAppRunner implements ApplicationRunner {
+
+    private final TitleEnhancer titleEnhancer;
+
+    private final BookRepository bookRepository;
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public BooksAppRunner(TitleEnhancer titleEnhancer, BookRepository bookRepository) {
+        this.titleEnhancer = titleEnhancer;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.info("📚 BookRepository: " + bookRepository.getClass().getSimpleName());
+        logger.info("↗️ TitleEnhancer: " + titleEnhancer.getClass().getSimpleName());
+    }
+}
