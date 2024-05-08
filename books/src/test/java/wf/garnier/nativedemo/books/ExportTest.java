ae1466439ddce54602c2273ed6449ac023801a7f
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-09 00:35:53 +0200

    tests: add a test for book exporting

---
+++ books/src/test/java/wf/garnier/nativedemo/books/ExportTest.java
@@ -0,0 +1,34 @@
+package wf.garnier.nativedemo.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ExportTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void export() throws Exception {
+        var contentLength = mockMvc.perform(get("/books/export"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .andReturn()
+                .getResponse()
+                .getContentLength();
+
+        assertThat(contentLength).isGreaterThan(3000);
+    }
+
+}
