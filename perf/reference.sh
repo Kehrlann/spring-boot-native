bcc3ee472061007d21416d42e12a750190d4768f
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-25 09:42:53 +0200

    perf: update reference script

--- perf/reference.sh
+++ perf/reference.sh
@@ -2,9 +2,17 @@
 
 exit 0
 
-poetry run locust --processes 4 --users 16
+poetry run locust --processes 4 --users 4 --spawn-rate 4
 
-poetry run locust --processes 4 --users 32 --headless --timescale --pghost=localhost --pgport=5432 --pgpassword=password --pguser=postgres
+poetry run locust \
+  --locustfile locustfile_parallel.py \
+  --processes 4 \
+  --users 8 \
+  --spawn-rate 8 \
+  --headless \
+  --timescale \
+  --pghost=localhost --pgport=5432 --pgpassword=password --pguser=postgres
+
 
 poetry run locust-compose up
 poetry run locust-compose down -v
