7077378429881d4207c81bcc93573de4b62a43a1
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-25 14:15:09 +0200

    examples: remove reflection hints

--- examples/src/main/java/wf/garnier/nativedemo/examples/ExampleApplication.java
+++ examples/src/main/java/wf/garnier/nativedemo/examples/ExampleApplication.java
@@ -1,15 +1,9 @@
 package wf.garnier.nativedemo.examples;
 
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.context.annotation.ReflectiveScan;
 
 @SpringBootApplication
-@ImportRuntimeHints(ExamplesRuntimeHints.class)
-@RegisterReflectionForBinding(TemplatingController.Book.class)
-@ReflectiveScan
 public class ExampleApplication {
 
 	public static void main(String[] args) {
