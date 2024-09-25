79e031fc3f5c1350293492b3b72b9bf21076ffcb
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-25 13:58:25 +0200

    build: bump Boot 3.4.0-M2 -> 3.4.0-M3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("org.springframework.boot") version "3.4.0-M2"
+        id("org.springframework.boot") version "3.4.0-M3"
         id("io.spring.dependency-management") version "1.1.5"
         id("io.spring.javaformat") version "0.0.43"
         id("org.graalvm.buildtools.native") version "0.10.3"
