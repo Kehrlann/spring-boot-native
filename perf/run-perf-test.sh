df08257f7e7b760adff28a03e9be238a2efa6559
Daniel Garnier-Moiroux <git@garnier.wf>
2024-09-26 14:52:21 +0200

    perf: test CPU limit is 2, use 16 users

--- perf/run-perf-test.sh
+++ perf/run-perf-test.sh
@@ -7,9 +7,9 @@ curl -s http://localhost:9001
 
 poetry run locust \
   --locustfile locustfile_parallel.py \
-  --processes 4 \
-  --users 8 \
-  --spawn-rate 8 \
+  --processes 6 \
+  --users 16 \
+  --spawn-rate 16 \
   --headless \
   --timescale \
   --pghost=localhost --pgport=5432 --pgpassword=password --pguser=postgres
--- run.sh
+++ run.sh
@@ -7,13 +7,16 @@ set +u
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 set -u
 
+# CPU limit
+LIMIT=2
+
 # GraalVM 22
 sdk use java 23-graal
 
-SERVER_PORT=9000 cpulimit -l 1 java -jar artifacts/books-app.jar &
+SERVER_PORT=9000 cpulimit -l "$LIMIT" java -jar artifacts/books-app.jar &
 JAVA_PID=$!
 
-SERVER_PORT=9001 cpulimit -l 1 ./artifacts/books-app
+SERVER_PORT=9001 cpulimit -l "$LIMIT" ./artifacts/books-app
 NATIVE_PID=$!
 
 trap 'pkill -P $$; exit' SIGINT SIGTERM EXIT
